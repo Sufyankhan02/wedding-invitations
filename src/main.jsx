98e429ecbfcd9ca30b462be953d13b972cbd162a
sufyan <khansufyan8408896553@gmail.com>
2026-01-31 17:38:45 +0530

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 import './index.css'
 import { InvitationProvider } from './context/InvitationContext'
@@ -26,6 +27,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <InvitationProvider>
         <App />
+        <Analytics />
       </InvitationProvider>
     </BrowserRouter>
   </StrictMode>,
